86b9ec6025db54b7197c4a20f501029c146c3e3e
Raphael ROMEO <95073137+Raph-Romeo@users.noreply.github.com>
2025-01-15 17:45:25 +0100

    0.0.5 - Frontend updates
    Small adjustments to side-panel drag interaction to feel more reactive and natural.
    Correctly implemented line break on textArea send message input.

--- main/static/scripts/app.js
+++ main/static/scripts/app.js
@@ -3,12 +3,12 @@ const application = document.getElementById("application-wrapper");
 let mouse_down_side_panel_drag_handle_initial_pos = null;
 let mouse_down_side_panel_drag_handle_initial_width = null;
 let max_side_panel_width = null;
-let minimum_side_panel_width = 180;
+let minimum_side_panel_width = 215;
 let compact_side_panel_mode = false;
-
+let app_fullscreen = false;
 
 if (window.innerWidth < 800){
-    max_side_panel_width = 350;
+    max_side_panel_width = 360;
 }else{
     max_side_panel_width = window.innerWidth/3;
 }
@@ -16,7 +16,7 @@ if (window.innerWidth < 800){
 let is_max_width = false;
 let mouse_down_side_panel_drag_handle = false;
 let current_side_panel_size = null;
-set_side_panel_width(350);
+set_side_panel_width(360);
 
 if (current_side_panel_size > max_side_panel_width){
     current_side_panel_size = max_side_panel_width;
@@ -96,6 +96,7 @@ function set_side_panel_compact(b){
 }
 
 function set_side_panel_fullscreen(b){
+    app_fullscreen = b;
     if (b){
         application.classList.add("page-view-full-screen");
     }else{
@@ -111,16 +112,17 @@ function set_side_panel_width(c){
             release_mouse_down_side_panel_drag_handle();
             set_side_panel_fullscreen(true);
             return;
-        }else if (c < 120 && !compact_side_panel_mode){
+        }else if (c < 74 + 50 && !compact_side_panel_mode){
             enable_animations(true);
             compact_side_panel_mode = true;
-            application.style.setProperty('--side-panel-extended-width', 180 + "px");
+            application.style.setProperty('--side-panel-extended-width', minimum_side_panel_width + "px");
             current_side_panel_size = 74;
             set_side_panel_compact(true);
             return;
-        }else if (c > 180 && compact_side_panel_mode){
+        }else if (c >= 74 + 50 && compact_side_panel_mode){
+            enable_animations(true);
             set_side_panel_compact(false);
-            enable_animations(false);
+			setTimeout(function(){enable_animations(false)}, 200);
             compact_side_panel_mode = false;
             current_side_panel_size = minimum_side_panel_width;
             application.style.setProperty('--side-panel-extended-width', minimum_side_panel_width + "px");
@@ -129,10 +131,28 @@ function set_side_panel_width(c){
             application.style.setProperty('--side-panel-extended-width', minimum_side_panel_width + "px");
         }
     }else if(c > max_side_panel_width){
+        if (!mouse_down_side_panel_drag_handle){
+            enable_animations(false);
+            setTimeout(function(){enable_animations(true)}, 1);
+        }
         current_side_panel_size = max_side_panel_width;
         application.style.setProperty('--side-panel-extended-width', max_side_panel_width + "px");
     }else{
+        enable_animations(false);
         current_side_panel_size = c;
         application.style.setProperty('--side-panel-extended-width', c + "px");
     }
 }
+
+/* Textarea */
+
+document.querySelectorAll("textarea").forEach(function(textarea) {
+  textarea.style.height = textarea.scrollHeight - 20 + "px";
+
+  textarea.addEventListener("input", function() {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+  });
+});
+
+setTimeout(function(){application.classList.remove("bottom-panel-hidden")}, 200);
