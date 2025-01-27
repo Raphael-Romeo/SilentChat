3dfec5711c7849a7ace0b0b8ef23f45c9fab9802
Raphael ROMEO <95073137+Raph-Romeo@users.noreply.github.com>
2025-01-27 21:43:42 +0100

    1.0.1 - Testing with debug mode True

--- chat/settings.py
+++ chat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z+o)k^^c$4hohikll%q%v@u%v2(-p&yqpw2z!9+dz8b06o))g6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
